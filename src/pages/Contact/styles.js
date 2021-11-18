08051894b99a7380e37f17dc039c3dbd174f9381
stwgabriel <gabrielstw@pm.me>
2021-11-17 21:06:55 -0300

    upload build

--- src/pages/Contact/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const MainView = styled.div`
-
-  width:100%;
-`;
-
-export { MainView };
--- src/pages/Portfolio/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const MainView = styled.div`
-
-  flex: 1;
-  width:100%;
-`;
-
-export { MainView };
